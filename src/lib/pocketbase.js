d665185eed52472911d09ae9bfc02c3a473e00d1
Barton Hammond <bartonhammond@gmail.com>
2025-01-09 07:02:36 -0600

    View contact
    Update the tsconfig so that the `paths` are defined

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -6,21 +6,50 @@ const pb = new PocketBase('http://127.0.0.1:8090');
 pb.autoCancellation(false)
 
 export async function getContacts({ q = null }) {
-  const options = {
-    filter: '',
-  }
+    const options = {
+	filter: '',
+    }
 
-  if (q) {
-    options.filter = `first ~ "${q}" || last ~ "${q}" || email ~ "${q}" || phone ~ "${q}"`
-  }
+    if (q) {
+	options.filter = `first ~ "${q}" || last ~ "${q}" || email ~ "${q}" || phone ~ "${q}"`
+    }
 
     let contacts = []
     
+    try {
+	contacts = await pb.collection('contacts').getFullList(options)
+	console.log(contacts)
+    } catch (e) {
+	console.log(e.response)
+    }
+    return contacts
+}
+
+export async function addContact({ first, last, phone, email }) {
+    let newContact
+    
+    try {
+	newContact = await pb.collection('contacts').create({
+	    first,
+	    last,
+	    phone,
+	    email,
+	})
+    } catch (e) {
+	console.log(e.response)
+    }
+    return newContact
+}
+
+export async function getContact(id) {
+  let contact
   try {
-    contacts = await pb.collection('contacts').getFullList(options)
-    console.log(contacts)
+    const options = {}
+    contact = await pb.collection('contacts').getOne(id, options)
   } catch (e) {
     console.log(e.response)
   }
-  return contacts
+
+  return contact
 }
+
