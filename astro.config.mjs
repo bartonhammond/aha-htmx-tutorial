6aafeff3fe31c71bbedfbce3a0dca12152ec8bfb
Barton Hammond <bartonhammond@gmail.com>
2025-01-11 17:42:22 -0600

    Show delete bulk button if at least 1 checkbox is checked
    This is using AlpineJS.  Follow directions here to add https://docs.astro.build/en/guides/integrations-guide/alpinejs/

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node'
+import alpinejs from '@astrojs/alpinejs';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: node({
@@ -17,5 +17,5 @@ export default defineConfig({
       Expires: '0',
     },
   },
+  integrations: [alpinejs()],
 })
-
